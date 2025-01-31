a27764bd6a120535858db71052304a8c2e3085fe
Rashmi Singh <rashsin2@cisco.com>
2025-01-31 09:55:09 +0530

    Resolved _02_06 and _03_02 Exercise

--- _02_06/TicketMachine.java
+++ _02_06/TicketMachine.java
@@ -1,21 +1,30 @@
 package _02_06;
 
+import _02_04.Ticket;
+
 public class TicketMachine {
   
 public static void main(String[] args) {
   // Create an object called ticket of type Ticket
+    Ticket ticket = new Ticket();
 
   // Set the destination of the ticket to New York
+    ticket.setDestination("New York");
 
   // Set the price of the ticket to 15.30
+    ticket.setPrice(15.30);
 
   // Set the isReturn value to true
+    ticket.setReturn(true);
 
   // Print the ticket's destination to the console
+    System.out.println(ticket.getDestination());
 
   // Print the ticket's price to the console
+    System.out.println(ticket.getPrice());
 
   // Print the ticket's isReturn value to the console
+    System.out.println(ticket.isReturn());
 
 }
 
